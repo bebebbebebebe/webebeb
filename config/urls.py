3e7076711b2c0c3b57c9dc5586863b69c3299d33
W W <wondloness@gmail.com>
2026-02-08 00:26:34 +0700

    4 LABA

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,20 @@ from django.contrib import admin
 from django.urls import path
 # Импортируем нашу функцию из приложения gallery
 from gallery.views import home
+from gallery.views import about
+from gallery.views import upload
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Пустая строка '' означает главную страницу сайта (http://localhost:8000/)
     path('', home, name='home'),
+    path('about/', about, name='home'),
+    path('upload/', upload, name='home')
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gallery/views.py
+++ gallery/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
+from .models import Asset # Импортируем модель, чтобы спрашивать данные
 def home(request):
-# Имитация данных из базы (список словарей)
-    fake_database = [
-        {'id': 1, 'name': 'Sci-Fi Helmet', 'file_size': '15 MB'},
-        {'id': 2, 'name': 'Old Chair', 'file_size': '2 MB'},
-        {'id': 3, 'name': 'Cyber Truck', 'file_size': '10 MB'},
-    ]
+# all() возвращает хаос.
+# order_by('-created_at') сортирует по полю created_at.
+# Минус (-) означает "по убыванию" (DESC).
+    assets = Asset.objects.all().order_by('-created_at')
     context_data = {
         'page_title': 'Главная Галерея',
-        'assets': fake_database, # Передаем весь список
+        'assets': assets,
     }
     return render(request, 'gallery/index.html', context_data)
 
+def about(request):
+# Мы пока не используем HTML-шаблоны, просто вернем строку.
+   
+    context_data = {
+        'page_title': 'О проекте',
+    }
+    return render(request, 'gallery/about.html', context_data)
+
+
+def upload(request):
+# Мы пока не используем HTML-шаблоны, просто вернем строку.
+   
+    context_data = {
+        'page_title': 'Загрузка кота',
+    }
+    return render(request, 'gallery/upload.html', context_data)
